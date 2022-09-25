99f17a9dbedf00cd894f015dadeeab20268ff2de
HunterBowie <73845280+HunterBowie@users.noreply.github.com>
2022-09-25 11:21:15 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import random as r
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-number = r.randint(0, 9)
-character = r.choice(["~","!","@","#","$","%","^","&","*","(",")","-","+"])
-letter = r.choice(alphabet).upper()
-
-print(str(number) + character + letter)
